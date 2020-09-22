7995238f62de608b29cb61ad1a1df2c288c3dd8b
crazvik <crazvik@gmail.com>
2020-09-22 19:29:35 +0200

    Update index.js

--- project/index.js
+++ project/index.js
@@ -1,38 +1,31 @@
+const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const queryString = require('querystring');
-const qs = queryString.parse('key=ALBATROSS');
-console.log(qs.key);
-let express = require('express');
-const e = require('express');
-let app = express();
 
-app.use(express.static(__dirname + '/project'))
-
-app.get('/', (req, res) => {
-  fs.readFile('public.txt', 'utf8', (e, data) => {
-    if(e) throw e;
-    res.send('<p>' + data);
-  });
+http.createServer(response).listen(3000, () => {
+  console.log("Listening on 3000")
 });
 
+function response(req, res) {
+  var text = fs.readFileSync('public.txt', 'utf8');
 
-
-app.listen(3000, () => {
-  console.log("Listening on 3000")
-})
-
-/*
-http.createServer(function (req, res) {
-  var query = url.parse(req.url, true);
-  var filename = "." + query.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(3000);*/
+  if(req.url === '/favicon.ico' || req.url === '/') {
+    res.write('<p>' + text);
+    res.end();
+    return;
+  }
+  
+  let params = url.parse(req.url, true)
+  
+  if(params.pathname === '/secret') {
+    if(params.query.key == 'ALBATROSS') {
+      var text = fs.readFileSync('secret.html', 'utf8');
+      res.write(text);
+      } else {
+      res.writeHead(403);
+      res.write("Unauthorized")
+    }
+    res.end();
+    return;
+  }
+}
